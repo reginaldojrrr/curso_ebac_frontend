298a296826a11303cb90c94500abbf1d4507b64a
Reginaldo Junior <reginaldo.junior@live.com>
2023-08-22 00:36:56 -0300

    Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function compileSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+function imageCompress() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build.images'))
+}
+
+function minimizeJS() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compileSass));
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(imageCompress));
+    gulp.watch('./build/scripts/*.js', { ignoreInitial: false }, gulp.series(minimizeJS));
+}
